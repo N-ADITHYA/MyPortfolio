cb67bb9fb7392bde8f3ebe131ff302d108d224fe
Adithya N <nadithya666@gmail.com>
2025-07-07 06:47:07 +0530

    Let's see

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Depends
-from schemas import PostCreate, CreateUser
+from backend.schemas import PostCreate, CreateUser
 from backend.database import session_local, engine
 from backend import models
 from sqlalchemy.orm import Session
